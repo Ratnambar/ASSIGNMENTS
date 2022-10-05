f159ec8e4eb4d2757554d111b788c532b6acc6ba
Ratnambar <ratnambar123gupta@gmail.com>
2022-10-05 21:38:16 +0530

    file upload .

--- accounts/models.py
+++ accounts/models.py
@@ -10,11 +10,5 @@ class UploadImage(models.Model):
     name = models.CharField(max_length=100)
     image = models.FileField(upload_to='accounts/', blank=True, null=True)
 
-    # def validate_image(image):
-    #     filesize = image.file.size
-    #     mb = 1
-    #     if filesize > mb*1024:
-    #         raise ValidationErr("Max file size 5mb")
-
 
     
